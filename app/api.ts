0befb0d39817b3e8b08516489e79cec551b4fdf5
dekel1234 <yonatandek@gmail.com>
2026-01-08 21:45:59 +0200

    arrivals API WIP

--- app/api.ts
+++ app/api.ts
@@ -1,11 +1,20 @@
 import axios from 'axios';
 const XMLParser = require('react-xml-parser');
 
-const fetchURL = 'https://moovitapp.com/tripplan/israel-1/poi';
+const connectURL = 'https://moovitapp.com/tripplan/israel-1/poi';
+const arrivalsURL = 'https://moovitapp.com/api/lines/linesarrival';
 
 let config = {
   headers: {
     "host": "moovitapp.com",
+    "MOOVIT_USER_KEY": "",
+    "MOOVIT_CUSTOMER_ID": "",
+    "MOOVIT_APP_TYPE": "WEB_TRIP_PLANNER",
+    "MOOVIT_CLIENT_VERSION": "5.151.2/V567",
+    "MOOVIT_GTFS_LANGUAGE ": "HE",
+    "MOOVIT_METRO_ID": "1",
+    "MOOVIT_PHONE_TYPE": "2",
+    "Origin": "https://moovitapp.com",
   }
 }
 
@@ -20,17 +29,55 @@ function findElementById(node: any, id: string): any {
   return null;
 }
 
-axios.get(fetchURL, config).then(res => {
-  const xml = new XMLParser().parseFromString(res.data);
-  let element = findElementById(xml, 'serverApp-state');
-  let serverAppState = element && element.value ? JSON.parse(element.value + "") : null;
-  let langId = serverAppState.appState.global.selectedLocale.langId;
-  let customerId = serverAppState.appState.customer.currentCustomer.id;
-  let metroId = serverAppState.appState.customer.currentCustomer.defaultMetroId;
-  let key = JSON.stringify({customerId,langId, metroId});
-  let userKey = serverAppState.appState.user.metroCombinations[key].userKey;
 
-  console.log(key);
-  console.log(serverAppState.appState.user.metroCombinations);
-  console.log({ langId, customerId, metroId, userKey });
-}).catch(err => console.error(err));
+function connect() {
+
+  axios.get(connectURL, config).then(res => {
+    const xml = new XMLParser().parseFromString(res.data);
+    let element = findElementById(xml, 'serverApp-state');
+    var serverAppState: any = null;
+    for (let extraBrackets = 0; extraBrackets < 10; extraBrackets++) {
+      try {
+        serverAppState = element && element.value ? JSON.parse(element.value + "}".repeat(extraBrackets)) : null;
+        break;
+      } catch (e) {
+        console.log(extraBrackets);
+        continue;
+      }
+    }
+
+    let langId = serverAppState.appState.global.selectedLocale.langId;
+    let customerId = serverAppState.appState.customer.currentCustomer.id;
+    let metroId = serverAppState.appState.customer.currentCustomer.defaultMetroId;
+    let key = JSON.stringify({ customerId, langId, metroId });
+    let userKey = serverAppState.appState.user.metroCombinations[key].userKey;
+    console.log({customerId, userKey, metroId, langId});
+    config.headers["MOOVIT_USER_KEY"] = userKey;
+    config.headers["MOOVIT_CUSTOMER_ID"] = customerId;
+    return {customerId, userKey, metroId, langId};
+  }).catch(err => console.error(err));
+
+}
+
+let keys = connect();
+
+function getArrivals(stopId: number, lineIds: number[]) {
+  const lineStopPairs = lineIds.map(lineId => ({
+    lineId,
+    stopId
+  }));
+
+  const requestBody = {
+    params: {
+      lineStopPairs
+    }
+  };
+
+  console.log(requestBody);
+
+  axios.post(arrivalsURL, requestBody, config).then(res => {
+    console.log(res.data);
+  });
+}
+
+getArrivals(37803416, [2780631, 5522099]);
